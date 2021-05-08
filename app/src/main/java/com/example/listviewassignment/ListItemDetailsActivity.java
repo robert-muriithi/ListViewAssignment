dfa617bcb1bc8e923a95bb7b8864ae3c6cae42d0
robert-muriithi <njuguiinirobert@gmail.com>
2021-05-08 22:12:15 +0300

    Complete

---
+++ app/src/main/java/com/example/listviewassignment/ListItemDetailsActivity.java
@@ -0,0 +1,14 @@
+package com.example.listviewassignment;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ListItemDetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_item_details);
+    }
+}
